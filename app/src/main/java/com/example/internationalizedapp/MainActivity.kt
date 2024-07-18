2d2fb8e6ed0d9a3aaedf82df9a882a63a9cf9fb2
asafe alves lopes <asafelopesdev@outlook.com>
2024-07-18 20:58:44 -0300

    view to show message created

--- app/src/main/java/com/example/internationalizedapp/MainActivity.kt
+++ app/src/main/java/com/example/internationalizedapp/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.internationalizedapp.ui.theme.InternationalizedAppTheme
 
@@ -19,29 +21,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             InternationalizedAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ShowMessage(
+                    message = stringResource(id = R.string.welcome_message)
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ShowMessage(message: String) {
+    Box(
+        contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = message, style = MaterialTheme.typography.titleLarge)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ShowMessagePreview() {
     InternationalizedAppTheme {
-        Greeting("Android")
+        ShowMessage(
+            message = stringResource(id = R.string.welcome_message)
+        )
     }
 }
